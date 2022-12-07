59b1ce4502b55c368807d9553cb0a0a41f026cc3
Rustam Sharipov <ooslab@inbox.ru>
2022-12-07 18:40:07 +0300

    Испраление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,26 @@ const profileButton = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const profileName = document.querySelector('.profile__title');
-const profileJob = document.querySelector('profile__subtitle');
+const profileJob = document.querySelector('.profile__subtitle');
 const popupForm = document.querySelector('.popup__form');
-const namePopup = document.querySelector('.popup__input_name');
-const jobPopup = document.querySelector('.popup__input_job');
+const namePopup = document.querySelector('.form__input_value_name');
+const jobPopup = document.querySelector('.form__input_value_job');
 
 profileButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 
-function openPopup(event) {
+function openPopup() {
   popup.classList.add('popup_opened');
+  namePopup.value = profileName.textContent
+  jobPopup.value = profileJob.textContent
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function valuesSave(e) {
-  e.preventDefault();
+function valuesSave(evt) {
+  evt.preventDefault();
   profileName.textContent = namePopup.value;
   profileJob.textContent = jobPopup.value;
   popupRemove();
