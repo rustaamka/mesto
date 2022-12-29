d8e486309aba59089e4dea90c68fe725cbadd8cd
Rustam Sharipov <ooslab@inbox.ru>
2022-12-29 12:10:34 +0300

    5 sprint

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,143 @@ function valuesSave(evt) {
 }
 
 popupForm.addEventListener('submit', valuesSave);
+
+//5
+const buttonAddCardPopup = document.querySelector('.profile__button-rectangle');
+const popupAddCard = document.querySelector('.popup__add-cards');
+const popupAddCardName = document.querySelector('.popup__input_value_text');
+const popupAddCardLink = document.querySelector('.popup__input_value_link');
+const popupImage = document.querySelector('.popup-image');
+const popupInputImgText = document.querySelector('.popup-image__text');
+const popupImageOpen = document.querySelector('.popup-image__open');
+const popupCloseAddCard = document.querySelector('.popup__close_add');
+const popupCloseImg = document.querySelector('.popup-image__close');
+const popupFormAdd = document.querySelector('.popup__add-form');
+const btnSubmitFormAddCard = popupFormAdd.querySelector('.popup__save');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//открытие попапа для добавления карточки
+buttonAddCardPopup.addEventListener('click', () => {
+  popupAddCard.classList.add('popup_opened');
+});
+
+//закрытие попапа добавления карточки
+popupCloseAddCard.addEventListener('click', () => {
+  popupAddCard.classList.remove('popup_opened');
+});
+
+//добавление лайка
+const likeAddClick = (evt) => {
+  evt.target.classList.toggle('element__like_active');
+};
+
+const openBigImg = (img, text) => {
+  popupImage.classList.add('popup_opened');
+  popupImageOpen.src = img;
+  popupImageOpen.alt = text;
+  popupInputImgText.textContent = text;
+}
+
+
+//создание карточки
+const elementContainer = document.querySelector('.elements');
+const templateElements = document.querySelector('#template-elements');
+
+//открытие картинки
+const openNewCard = (name, link) => {
+  const newCardAdd = templateElements.content.querySelector('.element').cloneNode(true);
+
+  newCardAdd.querySelector('.element__text').textContent = name;
+  newCardAdd.querySelector('.element__image').alt = name;
+  newCardAdd.querySelector('.element__image').src = link;
+  
+
+  //like
+  newCardAdd.querySelector('.element__like').addEventListener('click', likeAddClick);
+
+  //deleteCard
+  const deleteCard = newCardAdd.querySelector('.element__delete');
+  deleteCard.addEventListener('click', () => {
+    newCardAdd.remove();
+  });
+
+  //большая картинка
+  newCardAdd.querySelector('.element__image').addEventListener('click', () => {
+    openBigImg(link, name);
+  });
+
+  return newCardAdd;
+};
+
+//добавление карты
+const addNewCard = (name, link) => {
+  elementContainer.prepend(openNewCard(name, link));
+};
+
+//вставка из массива
+initialCards.forEach ((element) => {
+  addNewCard(element.name, element.link);
+});
+
+//добавление в галерею
+
+const cardsSave = (evt) => {
+  evt.preventDefault();
+  addNewCard(popupAddCardName.value, popupAddCardLink.value);
+  popupAddCard.classList.remove('popup_opened');
+  popupAddCard.classList.remove('popup__add-form');
+  popupFormAdd.reset();
+};
+
+popupFormAdd.addEventListener('submit', cardsSave);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
